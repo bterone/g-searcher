82dafbf202f49e88cd12e5d29426c990c3704667
Terone <bterone@gmail.com>
2020-10-19 18:58:13 +0700

    Refactor test cases

--- test/g_searcher/accounts_test.exs
+++ test/g_searcher/accounts_test.exs
@@ -6,70 +6,26 @@ defmodule GSearcher.AccountsTest do
   describe "users" do
     alias GSearcher.Accounts.User
 
-    @valid_attrs %{
-      email: "some email",
-      encrypted_password: "some encrypted_password",
-      username: "some username"
-    }
-    @update_attrs %{
-      email: "some updated email",
-      encrypted_password: "some updated encrypted_password",
-      username: "some updated username"
-    }
-    @invalid_attrs %{email: nil, encrypted_password: nil, username: nil}
-
-    def user_fixture(attrs \\ %{}) do
-      {:ok, user} =
-        attrs
-        |> Enum.into(@valid_attrs)
-        |> Accounts.create_user()
-
-      user
-    end
-
-    test "list_users/0 returns all users" do
-      user = user_fixture()
-      assert Accounts.list_users() == [user]
-    end
-
     test "get_user!/1 returns the user with given id" do
-      user = user_fixture()
+      user = insert(:user, password: nil, password_confirmation: nil)
       assert Accounts.get_user!(user.id) == user
     end
 
     test "create_user/1 with valid data creates a user" do
-      assert {:ok, %User{} = user} = Accounts.create_user(@valid_attrs)
-      assert user.email == "some email"
-      assert user.encrypted_password == "some encrypted_password"
-      assert user.username == "some username"
+      user_params = params_for(:user, username: "Terone123")
+
+      assert {:ok, %User{} = user} = Accounts.create_user(user_params)
+      assert user.username == "Terone123"
+      assert user.encrypted_password
     end
 
     test "create_user/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = Accounts.create_user(@invalid_attrs)
-    end
-
-    test "update_user/2 with valid data updates the user" do
-      user = user_fixture()
-      assert {:ok, %User{} = user} = Accounts.update_user(user, @update_attrs)
-      assert user.email == "some updated email"
-      assert user.encrypted_password == "some updated encrypted_password"
-      assert user.username == "some updated username"
-    end
-
-    test "update_user/2 with invalid data returns error changeset" do
-      user = user_fixture()
-      assert {:error, %Ecto.Changeset{}} = Accounts.update_user(user, @invalid_attrs)
-      assert user == Accounts.get_user!(user.id)
-    end
-
-    test "delete_user/1 deletes the user" do
-      user = user_fixture()
-      assert {:ok, %User{}} = Accounts.delete_user(user)
-      assert_raise Ecto.NoResultsError, fn -> Accounts.get_user!(user.id) end
+      invalid_params = params_for(:user, password: nil, password_confirmation: nil)
+      assert {:error, %Ecto.Changeset{}} = Accounts.create_user(invalid_params)
     end
 
     test "change_user/1 returns a user changeset" do
-      user = user_fixture()
+      user = build(:user)
       assert %Ecto.Changeset{} = Accounts.change_user(user)
     end
   end
--- test/g_searcher_web/controllers/user_controller_test.exs
+++ test/g_searcher_web/controllers/user_controller_test.exs
@@ -1,25 +1,6 @@
 defmodule GSearcherWeb.UserControllerTest do
   use GSearcherWeb.ConnCase
 
-  alias GSearcher.Accounts
-
-  @create_attrs %{
-    email: "some email",
-    encrypted_password: "some encrypted_password",
-    username: "some username"
-  }
-  @update_attrs %{
-    email: "some updated email",
-    encrypted_password: "some updated encrypted_password",
-    username: "some updated username"
-  }
-  @invalid_attrs %{email: nil, encrypted_password: nil, username: nil}
-
-  def fixture(:user) do
-    {:ok, user} = Accounts.create_user(@create_attrs)
-    user
-  end
-
   describe "new user" do
     test "renders form", %{conn: conn} do
       conn = get(conn, Routes.user_path(conn, :new))
@@ -29,59 +10,18 @@ defmodule GSearcherWeb.UserControllerTest do
 
   describe "create user" do
     test "redirects to index when data is valid", %{conn: conn} do
-      conn = post(conn, Routes.user_path(conn, :create), user: @create_attrs)
+      attrs = params_for(:user, password: "Password123", password_confirmation: "Password123")
+      conn = post(conn, Routes.user_path(conn, :create), user: attrs)
 
       assert redirected_to(conn) == Routes.page_path(conn, :index)
-      assert html_response(conn, 200) =~ "Show User"
+      assert get_flash(conn, :info) == "User created successfully."
     end
 
     test "renders errors when data is invalid", %{conn: conn} do
-      conn = post(conn, Routes.user_path(conn, :create), user: @invalid_attrs)
+      attrs = params_for(:user, password: nil, password_confirmation: nil)
+
+      conn = post(conn, Routes.user_path(conn, :create), user: attrs)
       assert html_response(conn, 200) =~ "New User"
     end
-  end
-
-  describe "edit user" do
-    setup [:create_user]
-
-    test "renders form for editing chosen user", %{conn: conn, user: user} do
-      conn = get(conn, Routes.user_path(conn, :edit, user))
-      assert html_response(conn, 200) =~ "Edit User"
-    end
-  end
-
-  describe "update user" do
-    setup [:create_user]
-
-    test "redirects when data is valid", %{conn: conn, user: user} do
-      conn = put(conn, Routes.user_path(conn, :update, user), user: @update_attrs)
-      assert redirected_to(conn) == Routes.user_path(conn, :show, user)
-
-      conn = get(conn, Routes.user_path(conn, :show, user))
-      assert html_response(conn, 200) =~ "some updated email"
-    end
-
-    test "renders errors when data is invalid", %{conn: conn, user: user} do
-      conn = put(conn, Routes.user_path(conn, :update, user), user: @invalid_attrs)
-      assert html_response(conn, 200) =~ "Edit User"
-    end
-  end
-
-  describe "delete user" do
-    setup [:create_user]
-
-    test "deletes chosen user", %{conn: conn, user: user} do
-      conn = delete(conn, Routes.user_path(conn, :delete, user))
-      assert redirected_to(conn) == Routes.page_path(conn, :index)
-
-      assert_error_sent 404, fn ->
-        get(conn, Routes.page_path(conn, :index))
-      end
-    end
-  end
-
-  defp create_user(_) do
-    user = fixture(:user)
-    %{user: user}
   end
 end
