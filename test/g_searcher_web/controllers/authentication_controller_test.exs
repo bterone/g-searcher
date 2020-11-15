d8878eb6812920f63f4db7ca6bfd101f102da647
Terone <bterone@gmail.com>
2020-11-15 21:47:26 +0700

    Add tests for untested routes

--- test/g_searcher_web/controllers/authentication_controller_test.exs
+++ test/g_searcher_web/controllers/authentication_controller_test.exs
@@ -1,11 +1,23 @@
 defmodule GSearcherWeb.AuthenticationControllerTest do
   use GSearcherWeb.ConnCase
 
+  import Mox
+
   alias GSearcher.Accounts.User
   alias GSearcher.Repo
 
+  describe "get /:provider" do
+    test "redirects to oauth page", %{conn: conn} do
+      conn =
+        conn
+        |> get(Routes.authentication_path(conn, :request, "google"))
+
+      assert redirected_to(conn, 302) =~ "google.com"
+    end
+  end
+
   describe "get /:provider/callback" do
-    test "signs in user given a valid user", %{conn: conn} do
+    test "signs in user and redirects to homepage given a valid user", %{conn: conn} do
       user = insert(:user, provider: "google")
 
       conn =
@@ -17,7 +29,7 @@ defmodule GSearcherWeb.AuthenticationControllerTest do
       assert redirected_to(conn) == Routes.page_path(conn, :index)
     end
 
-    test "register user given valid params", %{conn: conn} do
+    test "register user and redirects to homepage given valid params", %{conn: conn} do
       user_params = build(:user)
 
       conn =
@@ -31,6 +43,20 @@ defmodule GSearcherWeb.AuthenticationControllerTest do
       [user_in_db] = Repo.all(User)
 
       assert user_in_db.token == user_params.token
+    end
+
+    # TODO: Create test for when User fails to insert
+  end
+
+  describe "delete /sign-out" do
+    test "redirects to homepage when session is deleted", %{conn: conn} do
+      conn =
+        conn
+        |> sign_in
+        |> delete(Routes.authentication_path(conn, :sign_out))
+
+      assert get_flash(conn, :info) == "Signed out successfully!"
+      assert redirected_to(conn) == Routes.page_path(conn, :index)
     end
   end
 end
