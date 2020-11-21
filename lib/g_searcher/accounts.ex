010227fde3554d34eacb67a25a5f7899c75d84d7
Terone <bterone@gmail.com>
2020-11-21 17:23:45 +0700

    Remove line in Accounts.ex

--- lib/g_searcher/accounts.ex
+++ lib/g_searcher/accounts.ex
@@ -3,7 +3,6 @@ defmodule GSearcher.Accounts do
   alias GSearcher.Repo
 
   def get_user_by_id(user_id), do: Repo.get_by(User, id: user_id)
-
   def get_user_by_email(email), do: Repo.get_by(User, email: email)
 
   def create_user(params) do
