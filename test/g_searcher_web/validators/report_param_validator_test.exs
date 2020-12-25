aabb0cda202597901995f5379d01828e2ba087c9
Terone <bterone@gmail.com>
2020-12-25 11:21:03 +0700

    Add report param validator

---
+++ test/g_searcher_web/validators/report_param_validator_test.exs
@@ -0,0 +1,39 @@
+defmodule GSearcher.ReportParamValidatorTest do
+  use GSearcher.DataCase
+
+  alias GSearcher.ReportParamValidator
+
+  describe "validate/2" do
+    test "returns {:ok, valid_params} given valid params" do
+      report = build(:report)
+
+      params = %{
+        "title" => "Test Report",
+        "csv" => %Plug.Upload{
+          path: report.csv_path,
+          filename: "test.csv",
+          content_type: "text/csv"
+        }
+      }
+
+      assert {:ok, %{csv: _, title: "Test Report"}} = ReportParamValidator.validate(params)
+    end
+
+    test "returns {:error, :invalid_params, changeset} given invalid params" do
+      params = %{
+        "title" => "An Accident",
+        "csv" => %Plug.Upload{
+          path: "invalid_path",
+          filename: "cat_picture.jpg",
+          content_type: "text/plain"
+        }
+      }
+
+      assert {:error, :invalid_params, changeset} = ReportParamValidator.validate(params)
+
+      assert errors_on(changeset) == %{
+               csv: ["is not a CSV"]
+             }
+    end
+  end
+end
