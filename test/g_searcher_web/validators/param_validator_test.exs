6997b6aa30467b1ff436fe816f0cd705ea69e450
Terone <bterone@gmail.com>
2020-12-28 10:36:37 +0700

    Update params validation test

---
+++ test/g_searcher_web/validators/param_validator_test.exs
@@ -0,0 +1,43 @@
+defmodule GSearcherWeb.Validators.ParamValidatorTest do
+  use GSearcher.DataCase
+
+  alias GSearcherWeb.Validators.ParamValidator
+
+  defmodule PersonTest do
+    use Ecto.Schema
+
+    import Ecto.Changeset
+
+    embedded_schema do
+      field :name, :string
+    end
+
+    def changeset(data \\ %__MODULE__{}, params) do
+      data
+      |> cast(params, [:name])
+      |> validate_required(:name)
+    end
+  end
+
+  describe "validate/2" do
+    test "returns {:ok, attrs} given valid params" do
+      params = %{
+        "name" => "John Smith"
+      }
+
+      assert ParamValidator.validate(params, for: PersonTest) === {:ok, %{name: "John Smith"}}
+    end
+
+    test "returns {:error, :invalid_params, changeset} given invalid params" do
+      params = %{
+        "name" => ""
+      }
+
+      assert {:error, :invalid_params, changeset} =
+               ParamValidator.validate(params, for: PersonTest)
+
+      assert changeset.valid? === false
+      assert changeset.action === :validate
+    end
+  end
+end
