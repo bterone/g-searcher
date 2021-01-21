7afce9070900c4d7859ba0080483577668cc847e
Terone <bterone@gmail.com>
2021-01-21 19:21:27 +0700

    Update layout body to display screen name

--- lib/g_searcher_web/views/layout_view.ex
+++ lib/g_searcher_web/views/layout_view.ex
@@ -1,3 +1,10 @@
 defmodule GSearcherWeb.LayoutView do
   use GSearcherWeb, :view
+
+  def class_name(conn) do
+    join_path(conn.path_info)
+  end
+
+  defp join_path(path) when is_list(path), do: Enum.join(path, "-")
+  defp join_path(path), do: path
 end
