27ac076db31193ce3115f1c672d3b8aaba3437ff
Terone <bterone@gmail.com>
2021-02-07 20:01:58 +0700

    [16] Add HTML view to search result

--- lib/g_searcher_web/controllers/search_result_controller.ex
+++ lib/g_searcher_web/controllers/search_result_controller.ex
@@ -15,4 +15,16 @@ defmodule GSearcherWeb.SearchResultController do
         ErrorHandler.render_error(conn, 404)
     end
   end
+
+  def result_snapshot(conn, %{"id" => search_result_id}) do
+    user = conn.assigns.current_user
+
+    case SearchResults.get_user_search_result(search_result_id, user.id) do
+      {:ok, search_result} ->
+        render(conn, "result_snapshot.html", search_result_html: search_result.html_cache)
+
+      {:error, :not_found} ->
+        ErrorHandler.render_error(conn, 404)
+    end
+  end
 end
