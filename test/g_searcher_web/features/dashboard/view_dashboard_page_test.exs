4e9eb31f4c888d7e07546c0f67f00eac8d438639
Terone <bterone@gmail.com>
2021-04-29 14:26:18 +0700

    [17] Update dashboard test

--- test/g_searcher_web/features/dashboard/view_dashboard_page_test.exs
+++ test/g_searcher_web/features/dashboard/view_dashboard_page_test.exs
@@ -2,9 +2,14 @@ defmodule GSearcherWeb.HomePage.ViewDashboardPageTest do
   use GSearcherWeb.FeatureCase
 
   @selectors %{
-    report_upload: ".report-upload",
-    report_list: ".report-index",
-    report_titles: ".table__item--report-title p"
+    form_report_title: "input#report_title",
+    form_report_csv: "input#report_csv",
+    form_report_submit: ".form-report__action button",
+    report_upload: ".upload-report",
+    report_list: ".reports-index",
+    report_titles: ".table__item--report-title p",
+    success_notification: ".alert--info",
+    upload_report_button: ".form-report__button"
   }
 
   feature "view user dashboard", %{session: session} do
@@ -40,5 +45,26 @@ defmodule GSearcherWeb.HomePage.ViewDashboardPageTest do
     |> visit(Routes.dashboard_path(GSearcherWeb.Endpoint, :index))
     |> assert_has(css(@selectors[:report_titles], text: parsing_report.title))
     |> assert_has(css(@selectors[:report_titles], text: completed_report.title))
+  end
+
+  feature "uploads report form when filling report details", %{session: session} do
+    user = insert(:user)
+
+    report_details = build(:report, title: "Example 1")
+
+    session
+    |> sign_in_as(user)
+    |> visit(Routes.dashboard_path(GSearcherWeb.Endpoint, :index))
+    |> upload_report(report_details)
+    |> assert_has(css(@selectors[:success_notification], text: "Report generated successfully"))
+    |> assert_has(css(@selectors[:report_titles], text: "Example 1"))
+  end
+
+  defp upload_report(page, report) do
+    page
+    |> click(css(@selectors[:upload_report_button]))
+    |> fill_in(css(@selectors[:form_report_title]), with: report.title)
+    |> attach_file(css(@selectors[:form_report_csv]), path: report.csv_path)
+    |> click(css(@selectors[:form_report_submit]))
   end
 end
