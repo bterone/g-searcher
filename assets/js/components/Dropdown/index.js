f46e11e4bee2492ad663ca0c919d9d53c2580da1
Terone <bterone@gmail.com>
2021-04-29 10:36:19 +0700

    [17] Add dropdown upload form to upload report in dashboard

---
+++ assets/js/components/Dropdown/index.js
@@ -0,0 +1,23 @@
+import { Dropdown as BootstrapDropdown } from '../../vendor/bootstrap'
+
+class Dropdown {
+  /**
+   * Initialize
+   */
+  constructor(elementRef) {
+    this.elementRef = elementRef;
+
+    this._setup();
+  }
+
+  // Private
+
+  /**
+   * Component bootstrapping actions.
+   */
+   _setup() {
+    this.dropdown = new BootstrapDropdown(this.elementRef);
+  }
+}
+
+export default Dropdown;
---
+++ assets/js/screens/Dashboard/index.js
@@ -0,0 +1,32 @@
+import Dropdown from '../../components/Dropdown'
+
+const SELECTOR = {
+  screen: 'body.dashboard',
+  uploadReportButton: '#uploadReport',
+}
+
+class DashboardScreen {
+  /**
+   * Initializer
+   */
+  constructor() {
+    this.uploadReportButton = document.querySelector(SELECTOR.uploadReportButton);
+
+    this._setup();
+  }
+
+  /**
+   * Component bootstrapping actions.
+   */
+   _setup() {
+    this.uploadDropdown = new Dropdown(this.uploadReportButton)
+   }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const isDashboardScreen = document.querySelector(SELECTOR.screen) !== null
+
+  if (isDashboardScreen) {
+    new DashboardScreen();
+  }
+});
