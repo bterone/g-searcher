08e9ef721d86ac02cac0461543474579533f8ea0
Terone <bterone@gmail.com>
2021-05-06 18:50:00 +0700

    [35] Update frontend to filter search results and place in their respective section

--- lib/g_searcher_web/views/search_result_view.ex
+++ lib/g_searcher_web/views/search_result_view.ex
@@ -1,3 +1,17 @@
 defmodule GSearcherWeb.SearchResultView do
   use GSearcherWeb, :view
+
+  def top_advertiser_urls(search_result_urls) do
+    Enum.filter(search_result_urls, fn result_url -> result_url.is_top_ad end)
+  end
+
+  def other_advertiser_urls(search_result_urls) do
+    Enum.filter(search_result_urls, fn result_url -> result_url.is_regular_ad end)
+  end
+
+  def non_ad_urls(search_result_urls) do
+    Enum.filter(search_result_urls, fn result_url ->
+      (result_url.is_top_ad || result_url.is_regular_ad) == false
+    end)
+  end
 end
