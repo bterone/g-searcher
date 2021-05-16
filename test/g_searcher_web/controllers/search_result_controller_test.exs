3804602b687551061111e8c6aaa569d69d7495a7
Terone <bterone@gmail.com>
2021-05-16 18:44:12 +0700

    [20] Add test cases

--- test/g_searcher_web/controllers/search_result_controller_test.exs
+++ test/g_searcher_web/controllers/search_result_controller_test.exs
@@ -1,6 +1,25 @@
 defmodule GSearcherWeb.SearchResultControllerTest do
   use GSearcherWeb.ConnCase
 
+  describe "index/2" do
+    test "renders search results belonging to user given valid params", %{conn: conn} do
+      user = insert(:user)
+      report = insert(:report, user: user)
+
+      search_result = insert(:search_result)
+
+      _report_search_result =
+        insert(:report_search_result, report: report, search_result: search_result)
+
+      conn =
+        conn
+        |> sign_in(user)
+        |> get(Routes.search_result_path(conn, :index), query: "Example")
+
+      assert html_response(conn, 200)
+    end
+  end
+
   describe "show/2" do
     test "renders search result given search result belongs to user", %{conn: conn} do
       user = insert(:user)
