cb43dc112660d3d8b0067f2e072e7c9fd71b305b
Terone <bterone@gmail.com>
2021-05-27 12:56:50 +0700

    [41] Add search helper to parse query

---
+++ lib/g_searcher_web/controllers/search_helper.ex
@@ -0,0 +1,53 @@
+defmodule GSearcherWeb.SearchHelper do
+  @valid_operators ["title", "url", "top_ads", "regular_ads", "status"]
+
+  def parse_query(query) when is_binary(query) do
+    query
+    |> split_by_space(ignore_in_double_quotes: true)
+    |> convert_search_options_to_map()
+    |> build_query_attributes()
+  end
+
+  defp split_by_space(query, ignore_in_double_quotes: true) when is_binary(query),
+    do: Regex.split(~r{\s+(?=([^"]*"[^"]*")*[^"]*$)}, query)
+
+  defp convert_search_options_to_map(query_list) when is_list(query_list) do
+    Enum.map(query_list, fn query ->
+      case String.split(query, ":", parts: 2) do
+        [search_operator, query_param] ->
+          convert_search_options_to_map(search_operator, query_param)
+
+        [result] ->
+          result
+      end
+    end)
+  end
+
+  defp convert_search_options_to_map(operator, query_param) when operator in @valid_operators do
+    trimmed_param = String.trim(query_param, "\"")
+
+    %{cast_operator(operator) => trimmed_param}
+  end
+
+  defp convert_search_options_to_map(operator, query_param),
+    do: operator <> ":" <> query_param
+
+  defp build_query_attributes(query_list) when is_list(query_list) do
+    query =
+      query_list
+      |> Enum.reject(&is_map/1)
+      |> Enum.join(" ")
+
+    search_params = Enum.filter(query_list, &is_map/1)
+
+    Enum.reduce([%{query: query}] ++ search_params, fn search_option, acc ->
+      Map.merge(search_option, acc)
+    end)
+  end
+
+  defp cast_operator("title"), do: :title
+  defp cast_operator("url"), do: :url
+  defp cast_operator("top_ads"), do: :top_ads
+  defp cast_operator("regular_ads"), do: :regular_ads
+  defp cast_operator("status"), do: :status
+end
---
+++ test/g_searcher_web/controllers/search_helper_test.exs
@@ -0,0 +1,38 @@
+defmodule GSearcherWeb.SearchHelperTest do
+  use GSearcherWeb.ConnCase
+
+  alias GSearcherWeb.SearchHelper
+
+  describe "parse_query/1" do
+    test "returns a filter map given some filter params string" do
+      query_1 = "Some String title:Example url:test.com"
+      query_2 = "Some title:Example String"
+
+      assert SearchHelper.parse_query(query_1) == %{
+               query: "Some String",
+               title: "Example",
+               url: "test.com"
+             }
+
+      assert SearchHelper.parse_query(query_2) == %{query: "Some String", title: "Example"}
+    end
+
+    test "returns the given query in a map given a regular string" do
+      query_1 = "A regular string with some : and /"
+      query_2 = "https://www.google.com/search?q=cats&oq=cats"
+
+      assert SearchHelper.parse_query(query_1) == %{query: query_1}
+      assert SearchHelper.parse_query(query_2) == %{query: query_2}
+    end
+
+    test "returns correct filter map given filter params with double quotes" do
+      query = "Some String title:\"Title Example\" url:\"https://www.test.com/\""
+
+      assert SearchHelper.parse_query(query) == %{
+               query: "Some String",
+               title: "Title Example",
+               url: "https://www.test.com/"
+             }
+    end
+  end
+end
